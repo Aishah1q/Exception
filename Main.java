7e78f34edb25a58dfef99aea70e0859413fbfcf3
Aishah1q <148686153+Aishah1q@users.noreply.github.com>
2023-11-01 17:56:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //2
+    /*    try {
+            int[] arr = {1, 1, 1, 3, 3, 5};
+            int number = input.nextInt();
+            int count = 0;
+            for (int i = 0; i < arr.length; i++)
+                if (number == arr[i])
+                    count = count + 1;
+            System.out.println(count);
+
+
+        } catch (InputMismatchException e) {
+            System.out.printf("you most enter Numbers");}*/
+
+
+
+       /* int[] arr1 = {1, 4, 17, 7, 25, 3, 100};
+        try{
+            int k =input.nextInt();
+            Arrays.sort(arr1);
+            for (int i = arr1.length - 1; i >= (arr1.length - k); i--)
+                System.out.println(arr1[i]);
+        }catch (InputMismatchException e ){
+            System.out.printf("you most enter a number");}
+        catch (ArrayIndexOutOfBoundsException e){
+        System.out.printf("Worng Number");}*/
+
+
+//8
+       /* try{
+            int number=input.nextInt();
+            int a = 0, b = 1, c;
+            if (number == 0) {
+                System.out.println(a);}
+            for (int i=2;i<=number;i++){
+                c=a+b;
+                a=b;
+                b=c;}
+            System.out.println(b);}
+        catch (InputMismatchException e){
+            System.out.println("you most enter number");}*/
+
+
+    }}
+
+
+
+
+
+
+
+
+
+
